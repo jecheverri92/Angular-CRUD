e251c8e777266d835d9494979649aff2dccb7b58
Julian <jecheverri92@gmail.com>
2019-01-13 16:58:25 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-crud';
+  title = 'Angular Crud';
+
+  msg:string = '';
+
+  employees = [{
+    name: 'Fazt',
+    position: 'Manager',
+    email: 'WWW@.com'
+  },{
+    name: 'Pedro',
+    position: 'Designer',
+    email: 'WWWwwwwwwwwwwwwww@.com'
+  },
+  {
+    name: 'Juan',
+    position: 'Programer',
+    email: 'WWW@.com'
+  }];
+
+  model:any = {
+
+  }
+
+  model2:any = {
+
+  }
+  hideUpdate:boolean = true;
+  addEmployee():void{
+    this.employees.push(this.model);
+    this.msg = 'Campo agregado'
+  }
+
+  deleteEmployee(i):void{
+    let answer = confirm('are you sure?')
+    if(answer){
+      this.employees.splice(i,1);
+    }
+    this.msg = 'Campo Borrado'
+  }
+  myvalue;
+
+  editEmployee(i):void{
+    this.hideUpdate = false;
+    this.model2.name = this.employees[i].name;
+    this.model2.position = this.employees[i].position;
+    this.model2.email = this.employees[i].email;
+    console.log(i)
+    this.myvalue = i
+  }
+
+  updateEmployee():void{
+    let i = this.myvalue;
+    this.employees[i] = this.model2;
+    this.model2 = {}
+    this.msg = 'Campo Actualizado'
+  }
+
+  closeAlert():void{
+    this.msg = ''
+  }
 }
